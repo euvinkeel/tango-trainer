1cec2fb8411fb40d9ef4f7a191b73b83315bdb4e
euvinkeel <euvinmkeel@gmail.com>
2025-02-07 14:41:35 -0800

    added posthog

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import posthog from 'posthog-js'
+
+posthog.init('phc_p12fKhJRgrhy3Gw5zanXDGtz6GbvK3xlMmE6Tcan41r',
+    {
+        api_host: 'https://us.i.posthog.com',
+        person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+    }
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
